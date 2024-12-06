eb02de037e39c85ea1426609ea45d8fa5d810280
adrian <pirmporn4278@gmail.com>
2024-12-06 16:58:19 +0800

    added responsiveness

---
+++ app/About/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Link href={'/'}><h1>Home</h1></Link>
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <div className="h-screen bg-black p-3 flex">
+      <div className="h-screen bg-black p-3 md:flex">
         <div className="w-full h-full flex justify-center items-center bg-no-repeat" style={{ background: `url('/1609133839871.jpg')`, backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover', backgroundPosition:'50% 50%',}}>
-
+        <Link href={'/About'}><h2 className="font-extrabold absolute bottom-3 left-6  text-black p-2 " style={{}}>About</h2></Link>
           <div className="p-3 bg-orange-950 rounded">
-            <h1 className=" font-extrabold text-white ">KIDS MEDIA</h1>
+          <h1 className=" font-extrabold text-white ">KIDS MEDIA</h1>
           </div>
         </div>
-
         <div className="w-full h-full bg-white flex justify-center items-center " style={{background: `url('rubbing-hands-emoji.avif')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' , backgroundPosition:'50% 50%',}}>
           <div className=" p-3 bg-black rounded">
           <h1 className="text-white font-bold text-xl">DIDDY&#39;S #1 Favorite Site</h1>
@@ -17,12 +18,12 @@ export default function Home() {
           <input type="type" 
           placeholder="Kids Online..."
           className="absolute top-10 left-10 border border-gray-300 p-2 rounded shadow-md
-  />"style={{background: `url('bbyoil.webp')`,}}
+          />"style={{background: `url('bbyoil.webp')`,}}
           />
           <a href="https://www.gettyimages.com/photos/p-diddy"
           target="_blank"
           rel="noopener noreferrer">
-          <button className="absolute top-90 left-9 border-spacing-0 border-zinc-900 text-white font-extrabold p-2 bg-black h-10 ">
+          <button className="absolute top-90 left-5 border-spacing-0 border-zinc-900 text-white font-extrabold p-2 bg-black h-10 ">
             Diddy Pics
           </button>
           </a>
